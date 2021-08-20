1306804f99f0e5e00a74ca602f1806277b8fe5ee
Drflow94 <37194393+Drflow94@users.noreply.github.com>
2021-08-20 22:02:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <head>
-  <meta http-equiv="refresh" content="0; URL=https://t.co/SJ6GICNCTo?amp=1" />
+  <meta http-equiv="refresh" content="0; URL=https://t.co/Yc7OTqMAYW?amp=1" />
 </head>
 <body>
 </body>
